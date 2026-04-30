b4c17ff12f63bb6e7af728e73a4b9285aa811617
muktabarzaki <jekkpramana@gmail.com>
2026-04-30 13:17:10 +0700

    Nambah Page Login + Layouting pages Login

--- frontend-pipeltycs/src/App.jsx
+++ frontend-pipeltycs/src/App.jsx
@@ -1,12 +1,5 @@
+import Login from "./pages/Login";
+
 export default function App() {
-  return (
-    <div className="min-h-screen bg-slate-100 flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-600 mb-4">
-        Halo PIPELYTCS! 🚀
-      </h1>
-      <p className="text-slate-600 font-medium">
-        Tailwind CSS sudah berhasil terpasang sempurna.
-      </p>
-    </div>
-  )
+  return <Login />;
 }
---
+++ frontend-pipeltycs/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+export default function Login() {
+    return(
+        <div className="min-h-screen bg-[#E7E7E7] flex items-center justify-center p-8 gap-20">
+            <div className="flex flex-col items-center w-full max-w-sm border-2 border-dashed border-blue-400 p-4">
+                <h1>[Bagian kiri: Logo Pipeltycs]</h1>
+            </div>
+            <div className="w-full max-w-md bg-white rounded-2xl shadow-xl =-8">
+                <h1>[Bagian kanan: Form Login]</h1>
+            </div>
+        </div>
+    )
+
+}
