55b0a28dc2d423716284c9171fd45a799b60884e
muktabarzaki <jekkpramana@gmail.com>
2026-05-07 16:53:57 +0700

    Misahin Dashboard sama Sidebar

---
+++ frontend-pipeltycs/src/pages/SalesInsigth.jsx
@@ -0,0 +1,12 @@
+import Layout from '../components/Layout';
+
+export default function SalesInsight() {
+    return (
+        <Layout>
+            <h1 className="text-2xl font-bold mb-6">Sales Insight</h1>
+            <p className="text-gray-500">
+                Di sinilah nanti kita akan meletakkan form tambah data harian dan grafik tren penjualan.
+            </p>
+        </Layout>
+    );
+}
